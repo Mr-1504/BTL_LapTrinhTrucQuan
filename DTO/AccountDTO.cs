190e70cc8aab42fd4582c6b71fecd918fd3da0fa
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-19 15:00:16 +0700

    Create: Constructor

--- DTO/AccountDTO.cs
+++ DTO/AccountDTO.cs
@@ -12,5 +12,10 @@ namespace DTO
         private string _passwordHash;
         public string Id { get { return _id; } set { _id = value; } }
         public string PasswordHash { get { return _passwordHash; } set { _passwordHash = value; } }
+        public AccountDTO(string employeeId, string hashedPassword)
+        {
+            Id = employeeId;
+            PasswordHash = hashedPassword;
+        }
     }
 }
