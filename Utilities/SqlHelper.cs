ecb769b443efd104437ef7f25e1aa8be44d4c4c4
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-19 14:57:45 +0700

    Update SqlHelper.cs

--- Utilities/SqlHelper.cs
+++ Utilities/SqlHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Utilities
 {
@@ -14,6 +16,93 @@ namespace Utilities
 
             return baseConnectionString.Replace("Data Source=;", $"Data Source={deviceName}\\SQLEXPRESS;")
                                        .Replace("Initial Catalog=;", $"Initial Catalog={databaseName};");
+        }
+
+        public static int ExecuteNonQuery(string query, object[] objects)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        int i = 0;
+                        string[] parameters = query.Split(' ');
+                        foreach (string param in parameters)
+                        {
+                            if (param[0] == '@')
+                                cmd.Parameters.AddWithValue(param, objects[i++]);
+
+                        }
+                        conn.Open();
+                        int result = cmd.ExecuteNonQuery();
+                        return result > 0 ? 1 : 0;
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                return -1;
+            }
+        }
+
+
+        public static int ExecuteScalar(string query, object[] objects)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        string[] parameters = query.Split(' ');
+                        int i = 0;
+                        foreach(string param in parameters)
+                        {
+                            if (param[0] == '@')
+                                cmd.Parameters.AddWithValue(param, objects[i++]);
+                        }
+                        conn.Open();
+                        return (int) cmd.ExecuteScalar();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public static DataTable ExecuteReader(string query, object[] objects)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        string[] parameters = query.Split(' ');
+                        int i = 0;
+                        foreach (string param in parameters)
+                        {
+                            if (param[0] == '@')
+                                cmd.Parameters.AddWithValue(param, objects[i++]);
+                        }
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            return dt;
+                        }
+                    }
+            }
+            catch (SqlException)
+            {
+                return null;
+            }
         }
     }
 }
