7e19392283c867dcda89716b709c0a43eb70e651
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-21 14:45:13 +0700

    Test Food

--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -17,22 +17,37 @@ namespace GUI
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show(SqlHelper.GetConnectionString(), "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
             int res = new Class1().IsExist("QL0001");
-            if (res == 1)
-            {
-                MessageBox.Show("Thanh cong", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            else if (res == 0)
-            {
-                MessageBox.Show("k", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            MessageBox.Show(res.ToString(), "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //if (res == 1)
+            //{
+            //    MessageBox.Show("Thanh cong", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+            //else if (res == 0)
+            //{
+            //    MessageBox.Show("k", "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-            }else
-            {
-                MessageBox.Show("loi" + res.ToString(), "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}else
+            //{
+            //    MessageBox.Show("loi" + res.ToString(), "thong bao", MessageBoxButtons.OK, MessageBoxIcon.Error);
 
-            }
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = new Class1().GetData();
         }
     }
 }
