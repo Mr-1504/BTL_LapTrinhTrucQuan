e9e407604cf7dfbb1561bd0f79cdef1b50907efb
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-21 10:20:40 +0700

    Create Config.cs

---
+++ Utilities/Config.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public enum Employee
+    {
+        [Description("MaNhanVien")]
+        EmployeeId,
+
+        [Description("TenNhanVien")]
+        Name,
+
+        [Description("GioiTinh")]
+        Gender,
+
+        [Description("NamSinh")]
+        Birthday,
+
+        [Description("QueQuan")]
+        Hometown,
+
+        [Description("DiaChi")]
+        Address,
+
+        [Description("DienThoai")]
+        NumberPhone
+    }
+
+    public static class Config
+    {
+        public static string GetEnumDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            if (field != null)
+            {
+                DescriptionAttribute attribute = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                if (attribute != null)
+                {
+                    return attribute.Description;
+                }
+            }
+            return value.ToString();
+        }
+    }
+}
