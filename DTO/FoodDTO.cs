4ade876bc25f3c7afaf0ddf1383216a82a6b8db8
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-21 14:48:02 +0700

    Update FoodDTO.cs

--- DTO/FoodDTO.cs
+++ DTO/FoodDTO.cs
@@ -6,9 +6,25 @@
         private string foodName;
         private string foodMaking;
         private int foodUnitPrice;
+
+        public FoodDTO(string foodName, string foodMaking, int foodUnitPrice)
+        {
+            FoodName = foodName;
+            FoodMaking = foodMaking;
+            FoodUnitPrice = foodUnitPrice;
+        }
+
+        public FoodDTO(string foodId = "", string foodName = "", string foodMaking = "", int foodUnitPrice = 0) 
+            : this(foodName, foodMaking, foodUnitPrice)
+        {
+            FoodId = foodId;
+        }
+
         public int FoodUnitPrice { get => foodUnitPrice; set => foodUnitPrice = value; }
         public string FoodId { get => foodId; set => foodId = value; }
         public string FoodName { get => foodName; set => foodName = value; }
         public string FoodMaking { get => foodMaking; set => foodMaking = value; }
+
+        
     }
 }
