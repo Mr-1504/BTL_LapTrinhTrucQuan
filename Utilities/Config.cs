1eaa8ecf1c2bd6f551d4689f5d4369cd1891e031
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-21 10:53:55 +0700

    Create: enum Gender

--- Utilities/Config.cs
+++ Utilities/Config.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Utilities
 {
@@ -30,6 +26,18 @@ namespace Utilities
 
         [Description("DienThoai")]
         NumberPhone
+    }
+
+    public enum Gender
+    {
+        [Description("Nam")]
+        Male,
+
+        [Description("Nữ")]
+        Female,
+
+        [Description("Khác")]
+        Other
     }
 
     public static class Config
