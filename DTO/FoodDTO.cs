a154780a631bf866106f9cee2c1cdf096204c413
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-21 11:00:16 +0700

    Create FoodDTO.cs

---
+++ DTO/FoodDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DTO
+{
+    public class FoodDTO
+    {
+        private string foodId;
+        private string foodName;
+        private string foodMaking;
+        private int foodUnitPrice;
+        public int FoodUnitPrice { get => foodUnitPrice; set => foodUnitPrice = value; }
+        public string FoodId { get => foodId; set => foodId = value; }
+        public string FoodName { get => foodName; set => foodName = value; }
+        public string FoodMaking { get => foodMaking; set => foodMaking = value; }
+    }
+}
