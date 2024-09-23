971250ac189b78beb6b1a75ae1882673492d47dd
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-24 00:20:59 +0700

    Create OrderDetailDTO.cs

---
+++ DTO/OrderDetailDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace DTO
+{
+    public class OrderDetailDTO
+    {
+        private string _orderId;
+        private string _foodId;
+        private int _quantity;
+
+        public OrderDetailDTO(string orderId = "", string foodId = "", int quantity = 0)
+        {
+            OrderId = orderId;
+            FoodId = foodId;
+            Quantity = quantity;
+        }
+
+        public string OrderId { get => _orderId; set => _orderId = value; }
+        public string FoodId { get => _foodId; set => _foodId = value; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+    }
+}
