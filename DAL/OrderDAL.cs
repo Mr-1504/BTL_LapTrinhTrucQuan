9b770ce2c46b540c767ba97a0703895d81c5370b
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-23 23:30:32 +0700

    Update OrderDAL.cs

--- DAL/OrderDAL.cs
+++ DAL/OrderDAL.cs
@@ -1,10 +1,5 @@
 ﻿using DTO;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Utilities;
 
 namespace DAL
