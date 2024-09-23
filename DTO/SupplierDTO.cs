052ceeee2b057e595244793e047df07d3f16b932
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-23 23:30:45 +0700

    Create SupplierDTO.cs

---
+++ DTO/SupplierDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace DTO
+{
+    public class SupplierDTO
+    {
+        private string _supplierId;
+        private string _supplierName;
+        private string _supplierAddress;
+        private string _supplierNumberPhone;
+
+        public SupplierDTO(string supplierName = "", string supplierAddress = "", string supplierNumberPhone = "")
+        {
+            SupplierName = supplierName;
+            SupplierAddress = supplierAddress;
+            SupplierNumberPhone = supplierNumberPhone;
+        }
+
+        public SupplierDTO(string supplierId = "", string supplierName = "", string supplierAddress = "", 
+            string supplierNumberPhone = "") : this (supplierName, supplierAddress, supplierNumberPhone)
+        {
+            SupplierId = supplierId;
+        }
+
+        public string SupplierId { get => _supplierId; set => _supplierId = value; }
+        public string SupplierName { get => _supplierName; set => _supplierName = value; }
+        public string SupplierAddress { get => _supplierAddress; set => _supplierAddress = value; }
+        public string SupplierNumberPhone { get => _supplierNumberPhone; set => _supplierNumberPhone = value; }
+    }
+}
