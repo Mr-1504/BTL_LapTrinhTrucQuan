ac4136b1c03bca194f663bee10f0dbd0c2b58877
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-24 00:20:56 +0700

    Create OrderDetailDAL.cs

---
+++ DAL/OrderDetailDAL.cs
@@ -0,0 +1,36 @@
+﻿using Utilities;
+using DTO;
+using System.Data;
+
+namespace DAL
+{
+    public class OrderDetailDAL
+    {
+        public int IsExitOrder(string orderId)
+        {
+            string query = "SELECT COUNT(*) FROM ChiTietDonHang WHERE MaDon = @OrderId";
+            return SqlHelper.ExecuteNonQuery(query, new object[] { orderId });
+        }
+
+        public int AddNewOrderDetail(OrderDetailDTO orderDetail)
+        {
+            string query = "INSERT INTO ChiTietDonHang(MaDon, MaMonAn, SoLuong) VALUES ( @orderId , @foodId , @quantity )";
+            return SqlHelper.ExecuteNonQuery(query, new object[] 
+                {orderDetail.OrderId, orderDetail.FoodId, orderDetail.Quantity});
+        } 
+
+        public int UpdateOrder(OrderDetailDTO orderDetail)
+        {
+            string query = "UPDATE ChiTietDonHang SET SoLuong = @quantity WHERE MaDon = @orderId AND MaMonAn = @foodId";
+            return SqlHelper.ExecuteNonQuery(query, new object[] 
+                { orderDetail.Quantity, orderDetail.OrderId, orderDetail.FoodId });
+        }
+
+        public DataTable GetOrderDetail()
+        {
+            string query = "SELECT * FROM ChiTietDonHang";
+            return SqlHelper.ExecuteReader(query, new object[] { });
+        }
+
+    }
+}
