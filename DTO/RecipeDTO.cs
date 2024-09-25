f65b79fb5d562cbc30284fce9bf2fc1f7ad37a53
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-25 16:56:12 +0700

    Create RecipeDTO.cs

---
+++ DTO/RecipeDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace DTO
+{
+    public class RecipeDTO
+    {
+        private string _foodId;
+        private string _ingredientId;
+        private int _quantity;
+
+        public RecipeDTO(string foodId, string ingredientId, int quantity)
+        {
+            FoodId = foodId;
+            IngredientId = ingredientId;
+            Quantity = quantity;
+        }
+
+        public string FoodId { get => _foodId; set => _foodId = value; }
+        public string IngredientId { get => _ingredientId; set => _ingredientId = value; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+    }
+}
