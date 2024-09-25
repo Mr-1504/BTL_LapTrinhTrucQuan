a66480b26d8861c26bb4c1dbabe62e782a08161d
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-25 19:50:58 +0700

    Test PurchaseInvoice for DAL and DTO

--- BLL/Class1.cs
+++ BLL/Class1.cs
@@ -7,12 +7,12 @@ namespace BLL
     {
         public int IsExist(string userID)
         {
-           // return new SupplierDAL().UpdateSupplier(new DTO.SupplierDTO("0002", "Vinaphone", "Nghe an", "0983387388"));
-           return new OrderDetailDAL().UpdateOrder(new DTO.OrderDetailDTO("0001", "XAO0001", 4));
+            //return new SupplierDAL().UpdateSupplier(new DTO.SupplierDTO("0002", "Vinaphone", "Nghe an", "0983387388"));
+            return new PurchaseInvoiceDAL().UpdatePurchaseInvoice(new DTO.PurchaseInvoiceDTO("0001", "QL0002", "0001", new System.DateTime(2024, 10, 5)));
         }
 
         public DataTable GetData() {
-            return new OrderDetailDAL().GetOrderDetail();
+            return new PurchaseInvoiceDAL().GetPurchaseInvoice(purchaseInvoiceId: "0001", supplierId: "0001", employeeId: "QL0002");
         }
     }
 }
