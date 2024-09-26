6ae3a3400d9983e78d52c19e9031271190b27563
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-26 15:48:56 +0700

    Update employeeId and add Status Abtribute

--- DTO/EmployeeDTO.cs
+++ DTO/EmployeeDTO.cs
@@ -12,16 +12,18 @@ namespace DTO
         private string _hometown;
         private string _address;
         private string _numberPhone;
+        private EmployeeStatus _status;
 
+        public string EmployeeId { get => _employeeId; set => _employeeId = value; }
         public string Name { get => _name; set => _name = value; }
         public Gender Gender { get => _gender; set => _gender = value; }
         public DateTime Birth { get => _birth; set => _birth = value; }
         public string Hometown { get => _hometown; set => _hometown = value; }
         public string Address { get => _address; set => _address = value; }
-        public string EmployeeId { get => _employeeId; set => _employeeId = value; }
         public string NumberPhone { get => _numberPhone; set => _numberPhone = value; }
+        public EmployeeStatus Status { get => _status; set => _status = value; }
 
-        public EmployeeDTO(string id = "", string name = "", Gender gender = Gender.Other, DateTime birth = default(DateTime), string hometown = "", string address = "", string numberPhone = "")
+        public EmployeeDTO(string id, string name, Gender gender, DateTime birth, string hometown, string address, string numberPhone, EmployeeStatus status)
         {
             EmployeeId = id;
             Name = name;
@@ -30,6 +32,18 @@ namespace DTO
             Hometown = hometown;
             NumberPhone = numberPhone;
             Address = address;
+            Status = status;
+        }
+
+        public EmployeeDTO(string name, Gender gender, DateTime birth, string hometown, string address, string numberPhone, EmployeeStatus status)
+        {
+            Name = name;
+            Gender = gender;
+            Birth = birth;
+            Hometown = hometown;
+            Address = address;
+            NumberPhone = numberPhone;
+            Status = status;
         }
     }
 }
