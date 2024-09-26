047ea4afb1e83bb4846e9f050d5e819478e65c34
Mr-1504 <truongvan.minh1504@gmail.com>
2024-09-26 14:38:40 +0700

    Create PurchaseInvoiceDetailDTO.cs

---
+++ DTO/PurchaseInvoiceDetailDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace DTO
+{
+    public class PurchaseInvoiceDetailDTO
+    {
+        private string _purchaseInvoiceId;
+        private string _ingredientId;
+        private int _quantity;
+        private int _unitPrice;
+
+        public PurchaseInvoiceDetailDTO(string purchaseInvoiceId, string ingredientId, int quantity, int unitPrice)
+        {
+            PurchaseInvoiceId = purchaseInvoiceId;
+            IngredientId = ingredientId;
+            Quantity = quantity;
+            UnitPrice = unitPrice;
+        }
+
+        public string PurchaseInvoiceId { get => _purchaseInvoiceId; set => _purchaseInvoiceId = value; }
+        public string IngredientId { get => _ingredientId; set => _ingredientId = value; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+        public int UnitPrice { get => _unitPrice; set => _unitPrice = value; }
+    }
+}
