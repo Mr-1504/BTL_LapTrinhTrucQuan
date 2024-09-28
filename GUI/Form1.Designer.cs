c45e95cf9477df7d07b82cb4ac91e42a28d1b04c
Dung Zan <phamtiendung2k4@gmail..com>
2024-09-28 17:52:42 +0700

    create Loading Form

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -35,9 +35,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 341);
+            this.button1.Location = new System.Drawing.Point(472, 420);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 39);
+            this.button1.Size = new System.Drawing.Size(120, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,19 +47,22 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(115, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(153, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(601, 262);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(801, 322);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
---
+++ GUI/LoadingForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadingForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pnProcessBar_Paint(object sender, PaintEventArgs e)
+        {
+            int borderRadius = 17;
+            Panel panel = sender as Panel;
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.StartFigure();
+                path.AddArc(new Rectangle(0, 0, borderRadius, borderRadius), 180, 90); // Bo góc trên trái
+                path.AddArc(new Rectangle(panel.Width - borderRadius, 0, borderRadius, borderRadius), 270, 90); // Bo góc trên phải
+                path.AddArc(new Rectangle(panel.Width - borderRadius, panel.Height - borderRadius, borderRadius, borderRadius), 0, 90); // Bo góc dưới phải
+                path.AddArc(new Rectangle(0, panel.Height - borderRadius, borderRadius, borderRadius), 90, 90); // Bo góc dưới trái
+                path.CloseFigure();
+
+                // Thiết lập vùng cho panel để tạo bo góc
+                panel.Region = new Region(path);
+
+            }
+        }
+
+
+
+        private void LoadingForm_Paint(object sender, PaintEventArgs e)
+        {
+            int borderRadius = 30; // Bán kính bo góc
+            Form form = sender as Form;
+
+            // Tạo GraphicsPath để bo góc
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(new Rectangle(0, 0, borderRadius, borderRadius), 180, 90); // Bo góc trên trái
+            path.AddArc(new Rectangle(form.Width - borderRadius, 0, borderRadius, borderRadius), 270, 90); // Bo góc trên phải
+            path.AddArc(new Rectangle(form.Width - borderRadius, form.Height - borderRadius, borderRadius, borderRadius), 0, 90); // Bo góc dưới phải
+            path.AddArc(new Rectangle(0, form.Height - borderRadius, borderRadius, borderRadius), 90, 90); // Bo góc dưới trái
+            path.CloseFigure();
+
+            // Thiết lập vùng bo tròn cho form
+            this.Region = new Region(path);
+        }
+
+        private void logoBEA_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnHeader_Paint(object sender, PaintEventArgs e)
+        {
+            int borderRadius = 10;
+            Panel panel = sender as Panel;
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                path.StartFigure();
+                path.AddArc(new Rectangle(0, 0, borderRadius, borderRadius), 180, 90); // Bo góc trên trái
+                path.AddArc(new Rectangle(panel.Width - borderRadius, 0, borderRadius, borderRadius), 270, 90); // Bo góc trên phải
+                path.AddArc(new Rectangle(panel.Width - borderRadius, panel.Height - borderRadius, borderRadius, borderRadius), 0, 90); // Bo góc dưới phải
+                path.AddArc(new Rectangle(0, panel.Height - borderRadius, borderRadius, borderRadius), 90, 90); // Bo góc dưới trái
+                path.CloseFigure();
+
+                // Thiết lập vùng cho panel để tạo bo góc
+                panel.Region = new Region(path);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pnProcessBar2.Width += 3;
+            lbProcessPercent.Text = ((pnProcessBar2.Width * 100) / pnProcessBar.Width).ToString() + "%";
+            if (pnProcessBar2.Width >= pnProcessBar.Width)
+            {
+                timer1.Stop();
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+        }
+    }
+}
