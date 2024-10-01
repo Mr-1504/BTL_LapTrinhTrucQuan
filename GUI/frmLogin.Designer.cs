eaa5ca98f18d1d9989224e4dac01966121cf5ccb
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-01 09:33:35 +0700

    Create frmLogin

---
+++ GUI/frmLogin.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace GUI
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.pnlUser = new System.Windows.Forms.Panel();
+            this.pnlLine1 = new System.Windows.Forms.Panel();
+            this.picUser = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.pnlPass = new System.Windows.Forms.Panel();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnSeen = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.picPassword = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.picTitle = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.timerNameMove = new System.Windows.Forms.Timer(this.components);
+            this.timerPassMove = new System.Windows.Forms.Timer(this.components);
+            this.pnlUser.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).BeginInit();
+            this.pnlPass.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSeen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.Color.White;
+            this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txtUsername.Location = new System.Drawing.Point(70, 18);
+            this.txtUsername.MaxLength = 20;
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(250, 14);
+            this.txtUsername.TabIndex = 6;
+            this.txtUsername.TabStop = false;
+            this.txtUsername.Leave += new System.EventHandler(this.TxtUsername_Leave);
+            // 
+            // pnlUser
+            // 
+            this.pnlUser.BackColor = System.Drawing.Color.Transparent;
+            this.pnlUser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlUser.BackgroundImage")));
+            this.pnlUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlUser.Controls.Add(this.pnlLine1);
+            this.pnlUser.Controls.Add(this.picUser);
+            this.pnlUser.Controls.Add(this.lblName);
+            this.pnlUser.Controls.Add(this.txtUsername);
+            this.pnlUser.Location = new System.Drawing.Point(59, 219);
+            this.pnlUser.Name = "pnlUser";
+            this.pnlUser.Size = new System.Drawing.Size(338, 50);
+            this.pnlUser.TabIndex = 12;
+            // 
+            // pnlLine1
+            // 
+            this.pnlLine1.BackColor = System.Drawing.Color.Black;
+            this.pnlLine1.Location = new System.Drawing.Point(50, 9);
+            this.pnlLine1.Name = "pnlLine1";
+            this.pnlLine1.Size = new System.Drawing.Size(2, 30);
+            this.pnlLine1.TabIndex = 8;
+            // 
+            // picUser
+            // 
+            this.picUser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picUser.BackgroundImage")));
+            this.picUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picUser.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.picUser.Location = new System.Drawing.Point(15, 6);
+            this.picUser.Name = "picUser";
+            this.picUser.Size = new System.Drawing.Size(24, 34);
+            this.picUser.TabIndex = 7;
+            this.picUser.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.BackColor = System.Drawing.Color.White;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblName.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblName.Location = new System.Drawing.Point(67, 18);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(234, 15);
+            this.lblName.TabIndex = 13;
+            this.lblName.Text = "Mã nhân viên";
+            this.lblName.Click += new System.EventHandler(this.lblName_Click);
+            // 
+            // pnlPass
+            // 
+            this.pnlPass.BackColor = System.Drawing.Color.White;
+            this.pnlPass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pnlPass.BackgroundImage")));
+            this.pnlPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pnlPass.Controls.Add(this.lblPassword);
+            this.pnlPass.Controls.Add(this.btnSeen);
+            this.pnlPass.Controls.Add(this.panel2);
+            this.pnlPass.Controls.Add(this.picPassword);
+            this.pnlPass.Controls.Add(this.txtPassword);
+            this.pnlPass.Location = new System.Drawing.Point(59, 285);
+            this.pnlPass.Name = "pnlPass";
+            this.pnlPass.Size = new System.Drawing.Size(338, 50);
+            this.pnlPass.TabIndex = 13;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.BackColor = System.Drawing.Color.White;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.lblPassword.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblPassword.Location = new System.Drawing.Point(67, 18);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(234, 15);
+            this.lblPassword.TabIndex = 14;
+            this.lblPassword.Text = "Mật khẩu";
+            this.lblPassword.Click += new System.EventHandler(this.lblPassword_Click);
+            // 
+            // btnSeen
+            // 
+            this.btnSeen.BackColor = System.Drawing.Color.Transparent;
+            this.btnSeen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSeen.BackgroundImage")));
+            this.btnSeen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSeen.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnSeen.Location = new System.Drawing.Point(307, 15);
+            this.btnSeen.Name = "btnSeen";
+            this.btnSeen.Size = new System.Drawing.Size(20, 20);
+            this.btnSeen.TabIndex = 9;
+            this.btnSeen.TabStop = false;
+            this.btnSeen.Visible = false;
+            this.btnSeen.Click += new System.EventHandler(this.BtnSeen_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Location = new System.Drawing.Point(50, 9);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2, 30);
+            this.panel2.TabIndex = 8;
+            // 
+            // picPassword
+            // 
+            this.picPassword.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picPassword.BackgroundImage")));
+            this.picPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picPassword.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.picPassword.Location = new System.Drawing.Point(15, 6);
+            this.picPassword.Name = "picPassword";
+            this.picPassword.Size = new System.Drawing.Size(24, 34);
+            this.picPassword.TabIndex = 7;
+            this.picPassword.TabStop = false;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.txtPassword.Location = new System.Drawing.Point(70, 18);
+            this.txtPassword.MaxLength = 20;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(230, 14);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.TabStop = false;
+            this.txtPassword.TextChanged += new System.EventHandler(this.TxtPassword_TextChanged);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // picTitle
+            // 
+            this.picTitle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picTitle.Image = global::GUI.Properties.Resources.logo;
+            this.picTitle.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.picTitle.Location = new System.Drawing.Point(122, 38);
+            this.picTitle.Name = "picTitle";
+            this.picTitle.Size = new System.Drawing.Size(202, 59);
+            this.picTitle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picTitle.TabIndex = 14;
+            this.picTitle.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLogin.BackgroundImage")));
+            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.Green;
+            this.btnLogin.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.btnLogin.Location = new System.Drawing.Point(59, 350);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(338, 51);
+            this.btnLogin.TabIndex = 16;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Leelawadee UI", 21.75F);
+            this.lblTitle.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lblTitle.Location = new System.Drawing.Point(147, 148);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(157, 40);
+            this.lblTitle.TabIndex = 15;
+            this.lblTitle.Text = "Đăng nhập";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerNameMove
+            // 
+            this.timerNameMove.Interval = 15;
+            this.timerNameMove.Tick += new System.EventHandler(this.timerNameMove_Tick);
+            // 
+            // timerPassMove
+            // 
+            this.timerPassMove.Interval = 15;
+            this.timerPassMove.Tick += new System.EventHandler(this.timerPassMove_Tick);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(457, 469);
+            this.Controls.Add(this.pnlUser);
+            this.Controls.Add(this.pnlPass);
+            this.Controls.Add(this.picTitle);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.pnlUser.ResumeLayout(false);
+            this.pnlUser.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picUser)).EndInit();
+            this.pnlPass.ResumeLayout(false);
+            this.pnlPass.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSeen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTitle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Panel pnlUser;
+        private System.Windows.Forms.Panel pnlLine1;
+        private System.Windows.Forms.PictureBox picUser;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel pnlPass;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.PictureBox btnSeen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox picPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.PictureBox picTitle;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Timer timerNameMove;
+        private System.Windows.Forms.Timer timerPassMove;
+    }
+}
---
+++ GUI/frmLogin.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilities;
+
+namespace GUI
+{
+    public partial class frmLogin : Form
+    {
+        private bool isNameUp;
+        private bool isPasswordUp;
+        private bool seen;
+        //private ListBookForm listBookForm;
+        public frmLogin()
+        {
+            seen = false;
+            isNameUp = false;
+            isPasswordUp = false;
+            InitializeComponent();
+            lblName.Cursor = Cursors.IBeam;
+            lblPassword.Cursor = Cursors.IBeam;
+        }
+        
+
+        private void timerNameMove_Tick(object sender, EventArgs e)
+        {
+            if (!isNameUp)
+                if (lblName.Location.Y > -4)
+                {
+                    lblName.Location = new Point(lblName.Location.X, lblName.Location.Y - 2);
+                }
+                else
+                {
+                    timerNameMove.Stop();
+                    isNameUp = true;
+                    lblName.Cursor = Cursors.Default;
+                }
+            else
+                if (lblName.Location.Y < 18)
+            {
+                lblName.Location = new Point(lblName.Location.X, lblName.Location.Y + 2);
+            }
+            else
+            {
+                lblName.AutoSize = false;
+                lblName.Size = new Size(234, 15);
+                timerNameMove.Stop();
+                isNameUp = false;
+                lblName.Cursor = Cursors.IBeam;
+            }
+
+        }
+        private void timerPassMove_Tick(object sender, EventArgs e)
+        {
+            if (!isPasswordUp)
+                if (lblPassword.Location.Y > -4)
+                {
+                    lblPassword.Location = new Point(lblPassword.Location.X, lblPassword.Location.Y - 2);
+                }
+                else
+                {
+                    timerPassMove.Stop();
+                    isPasswordUp = true;
+                    lblPassword.Cursor = Cursors.Default;
+                }
+            else
+                if (lblPassword.Location.Y < 18)
+            {
+                lblPassword.Location = new Point(lblPassword.Location.X, lblPassword.Location.Y + 2);
+            }
+            else
+            {
+                lblPassword.AutoSize = false;
+                lblPassword.Size = new Size(234, 15);
+                timerPassMove.Stop();
+                isPasswordUp = false;
+                lblPassword.Cursor = Cursors.Default;
+            }
+        }
+        private void lblName_Click(object sender, EventArgs e)
+        {
+            if (!isNameUp)
+            {
+                lblName.AutoSize = true;
+                lblName.Text = "Mã nhân viên";
+                lblName.ForeColor = Color.Black;
+                txtUsername.Focus();
+                timerNameMove.Start();
+            }
+        }
+
+        private void TxtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                timerNameMove.Start();
+                lblName.Text = "Mã nhân viên";
+                lblName.ForeColor = Color.Black;
+            }
+        }
+
+        private void lblPassword_Click(object sender, EventArgs e)
+        {
+            if (!isPasswordUp)
+            {
+                txtPassword.Focus();
+                lblPassword.AutoSize = true;
+                lblPassword.Text = "Mật khẩu";
+                lblPassword.ForeColor = Color.Black;
+                timerPassMove.Start();
+            }
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "")
+            {
+                txtPassword.UseSystemPasswordChar = true;
+                timerPassMove.Start();
+            }
+        }
+
+        private void TxtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (!seen)
+            {
+                txtPassword.UseSystemPasswordChar = true;
+            }
+            if (txtPassword.Text == "")
+            {
+                btnSeen.Visible = false;
+            }
+            else
+            {
+                btnSeen.Visible = true;
+            }
+        }
+
+        private void BtnSeen_Click(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !txtPassword.UseSystemPasswordChar;
+
+            if (seen)
+            {
+                Image image = Image.FromFile("..\\Resources\\Seen.png");
+                btnSeen.BackgroundImage = image;
+            }
+            else
+            {
+                Image image = Image.FromFile("..\\Resources\\UnSeen.png");
+                btnSeen.BackgroundImage = image;
+            }
+            seen = !seen;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (this.txtUsername.Text == "")
+            {
+                lblName.Text = "Nhập đủ thông tin";
+                lblName.ForeColor = Color.Red;
+                return;
+            }
+            if (this.txtPassword.Text == "")
+            {
+                lblPassword.Text = "Nhập đủ thông tin";
+                lblPassword.ForeColor = Color.Red;
+                return;
+            }
+           // Login();
+        }
+        //private void Login()
+        //{
+        //    try
+        //    {
+        //        SqlHelper sqlHelper = new SqlHelper();
+        //        List<string> informationLogin = sqlHelper.GetLogin(this.txtUsername.Text);
+        //        if (informationLogin == null || informationLogin.Count < 2)
+        //        {
+        //            lblName.AutoSize = true;
+        //            lblName.Text = "Tên người dùng không đúng";
+        //            lblName.ForeColor = Color.Red;
+        //            lblName.AutoSize = false;
+        //            return;
+        //        }
+        //        if (this.IsPasswordValid(this.txtPassword.Text, informationLogin[1]))
+        //        {
+        //            MessageBox.Show("Đăng nhập thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //            this.txtPassword.Text = "";
+        //            this.txtUsername.Text = "";
+        //            timerNameMove.Start();
+        //            timerPassMove.Start();
+        //            this.seen = false;
+        //            this.txtPassword.UseSystemPasswordChar = false;
+        //            return;
+        //        }
+        //        else
+        //        {
+        //            lblPassword.AutoSize = true;
+        //            lblPassword.Text = "Mật khẩu không đúng";
+        //            lblPassword.ForeColor = Color.Red;
+        //            lblPassword.AutoSize = false;
+        //            return;
+        //        }
+        //    }
+        //    catch (SqlException)
+        //    {
+        //        MessageBox.Show("Lỗi kết nối cơ sở dữ liệu", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        private void picTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
