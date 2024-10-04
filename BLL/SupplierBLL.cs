abf0c1896a1018c2d74aa4187130a139818a01f2
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-04 12:33:06 +0700

    Create SupplierBLL.cs

---
+++ BLL/SupplierBLL.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using DTO;
+using System.Collections.Generic;
+using System.Data;
+using Utilities;
+namespace BLL
+{
+    public class SupplierBLL
+    {
+        public List<SupplierDTO> GetSuppliers()
+        {
+            List<SupplierDTO> suppliers = new List<SupplierDTO>();
+            DataTable data = new SupplierDAL().GetSuppliers();
+            foreach (DataRow row in data.Rows)
+            {
+                if (row != null)
+                {
+                    suppliers.Add(new SupplierDTO(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString(), Status.Use));
+                }
+            }
+            return suppliers;
+        }
+    }
+}
