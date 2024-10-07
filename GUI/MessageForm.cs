affcfffb9997f70ab679a1d88a4849ba285d0095
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-08 00:18:41 +0700

    Create MessageForm.cs

---
+++ GUI/MessageForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class MessageForm : Form
+    {
+        public MessageForm(string message, string title)
+        {
+            InitializeComponent();
+            lblTitle.Text = title;
+            lblMessage.Text = message;
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.No;
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            DialogResult= DialogResult.Yes;
+        }
+    }
+}
