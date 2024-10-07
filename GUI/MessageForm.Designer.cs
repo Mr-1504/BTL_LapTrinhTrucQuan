394ff941a112ade6c534c1597e62728c2e16f897
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-08 00:18:45 +0700

    Create MessageForm.Designer.cs

---
+++ GUI/MessageForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace GUI
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 30);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnNo
+            // 
+            this.btnNo.BackColor = System.Drawing.Color.Transparent;
+            this.btnNo.BackgroundImage = global::GUI.Properties.Resources.No;
+            this.btnNo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNo.FlatAppearance.BorderSize = 0;
+            this.btnNo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnNo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNo.Location = new System.Drawing.Point(51, 153);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(35, 35);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.UseVisualStyleBackColor = false;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnYes
+            // 
+            this.btnYes.BackColor = System.Drawing.Color.Transparent;
+            this.btnYes.BackgroundImage = global::GUI.Properties.Resources.Yes;
+            this.btnYes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnYes.FlatAppearance.BorderSize = 0;
+            this.btnYes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnYes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYes.Location = new System.Drawing.Point(318, 153);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(35, 35);
+            this.btnYes.TabIndex = 2;
+            this.btnYes.UseVisualStyleBackColor = false;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.lblTitle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 5);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(81, 19);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Xác nhận";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(41, 67);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(318, 36);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "Bạn chắc chắn muốn huỷ thêm hoá đơn bán?\nHành động này không thể hoàn tác.";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MessageForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MessageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
