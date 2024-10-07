c8558de2231755de4e39283ca736d52156e8aa5c
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-08 00:26:43 +0700

    Update BaseForm.Designer.cs

--- GUI/BaseForm.Designer.cs
+++ GUI/BaseForm.Designer.cs
@@ -788,7 +788,6 @@ namespace GUI
             this.picHome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picHome.TabIndex = 1;
             this.picHome.TabStop = false;
-            this.picHome.Click += new System.EventHandler(this.picHome_Click);
             // 
             // pnlContent
             // 
