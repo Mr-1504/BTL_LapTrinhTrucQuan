c757316537cd7a5de56a6060760185d5a235bc5c
watashiwalds <143416825+watashiwalds@users.noreply.github.com>
2024-10-11 10:44:03 +0700

    split BAL and DAL

--- BLL/WarehouseBLL.cs
+++ BLL/WarehouseBLL.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using static DAL.WarehouseDAL;
 using Utilities;
+using DAL;
 
 namespace BLL
 {
@@ -10,71 +12,32 @@ namespace BLL
     {
         public static class ListAll
         {
-            private static string TableQuery = "select MaNguyenLieu, TenNguyenLieu, DonViTinh, Soluong from NguyenLieu";
-            private static string SearchQuery = string.Empty;
-            private static string OrderQuery = string.Empty;
-
-            public static void BLL_SetValueDefault()
-            {
-                TableQuery = "select MaNguyenLieu, TenNguyenLieu, DonViTinh, Soluong from NguyenLieu";
-                SearchQuery = string.Empty;
-                OrderQuery = string.Empty;
-            }
-
             public static DataTable BLL_GetTable() 
-            { 
-                return SqlHelper.ExecuteReader(TableQuery + SearchQuery + OrderQuery, new object[] { }); 
+            {
+                return WarehouseDAL.ListAll.DAL_GetTable(); 
             }
             public static DataTable BLL_SearchKeywordInColumn(string colName = "", string keyword = "")
             {
-                if (!string.IsNullOrEmpty(colName) && !string.IsNullOrEmpty(keyword))
-                {
-                    SearchQuery = $" where {colName} like N'%{keyword}%'";
-                }
-                else
-                {
-                    SearchQuery = string.Empty;
-                }
-                return BLL_GetTable();
+                return WarehouseDAL.ListAll.DAL_SearchKeywordInColumn(colName, keyword);
             }
             public static DataTable BLL_OrderByColumn(string colName, Boolean ascend)
             {
-                OrderQuery = $" order by {colName} {(ascend ? "ASC" : "DESC")}";
-                return BLL_GetTable();
+                return WarehouseDAL.ListAll.DAL_OrderByColumn(colName, ascend);
             }
         }
         public static class ServingPrecal
         {
-            private static string MenuQuery = "select MaMonAn, TenMonAn, DonGia from MonAn where TrangThai = 1";
-            private static string SearchQuery = string.Empty;
-
             public static DataTable BLL_GetMenu()
             {
-                return SqlHelper.ExecuteReader(MenuQuery + SearchQuery, new object[] { });
+                return WarehouseDAL.ServingPrecal.DAL_GetMenu();
             }
             public static DataTable BLL_SearchFromMenu(string keyword)
             {
-                SearchQuery = string.IsNullOrEmpty(keyword) ? string.Empty : $" and (MaMonAn like N'%{keyword}%' or TenMonAn like N'%{keyword}%' or DonGia like N'%{keyword}%')";
-                return BLL_GetMenu();
+                return WarehouseDAL.ServingPrecal.DAL_SearchFromMenu(keyword);
             }
             public static void BLL_GetPrecalResult(List<string> dishIDs, ref List<int> out_dishCount, ref List<string> out_lowIngredientNames)
             {
-                string dishList = MakeStringForSQLQuery(dishIDs);
-                DataTable precalTable = SqlHelper.ExecuteReader(
-                    "with InStock as (" +
-                        "select NguyenLieu.TenNguyenLieu, NguyenLieu.MaNguyenLieu, NguyenLieu.SoLuong as TrongKho " +
-                        "from NguyenLieu join NguyenLieuMonAn on NguyenLieu.MaNguyenLieu = NguyenLieuMonAn.MaNguyenLieu " +
-                       $"where MaMonAn in {dishList} " +
-                    "), Requiring as ( " +
-                        "select MaNguyenLieu, sum(SoLuong) as CanDung " +
-                        "from NguyenLieuMonAn " +
-                       $"where MaMonAn in {dishList} " +
-                        "group by MaNguyenLieu " +
-                     ") " +
-                     "select InStock.MaNguyenLieu, min(cast((TrongKho / CanDung) as int)) as Precal, TenNguyenLieu " +
-                     "from InStock join Requiring on InStock.MaNguyenLieu = Requiring.MaNguyenLieu " +
-                     "group by TenNguyenLieu, InStock.MaNguyenLieu " +
-                     "order by Precal ASC", new object[] { });
+                DataTable precalTable = WarehouseDAL.ServingPrecal.DAL_GetPrecalResult(dishIDs);
 
                 //  * trong trường hợp precalTable có data == đang có món cần tính
                 /*
@@ -107,14 +70,6 @@ namespace BLL
                     }
                 }
             }
-        }
-
-        public static string MakeStringForSQLQuery(List<string> daList)
-        {
-            string res = "(";
-            foreach(string s in daList) res += "'" + s + "',";
-            res = res.Substring(0, res.Length - 1) + ")";
-            return res;
         }
     }
 }
---
+++ DAL/WarehouseDAL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Utilities;
+
+namespace DAL
+{
+    public static class WarehouseDAL
+    {
+        public static class ListAll
+        {
+            private static string TableQuery = "select MaNguyenLieu, TenNguyenLieu, DonViTinh, Soluong from NguyenLieu";
+            private static string SearchQuery = string.Empty;
+            private static string OrderQuery = string.Empty;
+
+            public static void DAL_SetValueDefault()
+            {
+                TableQuery = "select MaNguyenLieu, TenNguyenLieu, DonViTinh, Soluong from NguyenLieu";
+                SearchQuery = string.Empty;
+                OrderQuery = string.Empty;
+            }
+
+            public static DataTable DAL_GetTable()
+            {
+                return SqlHelper.ExecuteReader(TableQuery + SearchQuery + OrderQuery, new object[] { });
+            }
+            public static DataTable DAL_SearchKeywordInColumn(string colName = "", string keyword = "")
+            {
+                if (!string.IsNullOrEmpty(colName) && !string.IsNullOrEmpty(keyword))
+                {
+                    SearchQuery = $" where {colName} like N'%{keyword}%'";
+                }
+                else
+                {
+                    SearchQuery = string.Empty;
+                }
+                return DAL_GetTable();
+            }
+            public static DataTable DAL_OrderByColumn(string colName, Boolean ascend)
+            {
+                OrderQuery = $" order by {colName} {(ascend ? "ASC" : "DESC")}";
+                return DAL_GetTable();
+            }
+        }
+        public static class ServingPrecal
+        {
+            private static string MenuQuery = "select MaMonAn, TenMonAn, DonGia from MonAn where TrangThai = 1";
+            private static string SearchQuery = string.Empty;
+
+            public static DataTable DAL_GetMenu()
+            {
+                return SqlHelper.ExecuteReader(MenuQuery + SearchQuery, new object[] { });
+            }
+            public static DataTable DAL_SearchFromMenu(string keyword)
+            {
+                SearchQuery = string.IsNullOrEmpty(keyword) ? string.Empty : $" and (MaMonAn like N'%{keyword}%' or TenMonAn like N'%{keyword}%' or DonGia like N'%{keyword}%')";
+                return DAL_GetMenu();
+            }
+            public static DataTable DAL_GetPrecalResult(List<string> dishIDs)
+            {
+                string dishList = MakeStringForSQLQuery(dishIDs);
+                return SqlHelper.ExecuteReader(
+                    "with InStock as (" +
+                        "select NguyenLieu.TenNguyenLieu, NguyenLieu.MaNguyenLieu, NguyenLieu.SoLuong as TrongKho " +
+                        "from NguyenLieu join NguyenLieuMonAn on NguyenLieu.MaNguyenLieu = NguyenLieuMonAn.MaNguyenLieu " +
+                       $"where MaMonAn in {dishList} " +
+                    "), Requiring as ( " +
+                        "select MaNguyenLieu, sum(SoLuong) as CanDung " +
+                        "from NguyenLieuMonAn " +
+                       $"where MaMonAn in {dishList} " +
+                        "group by MaNguyenLieu " +
+                     ") " +
+                     "select InStock.MaNguyenLieu, min(cast((TrongKho / CanDung) as int)) as Precal, TenNguyenLieu " +
+                     "from InStock join Requiring on InStock.MaNguyenLieu = Requiring.MaNguyenLieu " +
+                     "group by TenNguyenLieu, InStock.MaNguyenLieu " +
+                     "order by Precal ASC", new object[] { });
+            }
+        }
+
+        public static string MakeStringForSQLQuery(List<string> daList)
+        {
+            string res = "(";
+            foreach (string s in daList) res += "'" + s + "',";
+            res = res.Substring(0, res.Length - 1) + ")";
+            return res;
+        }
+    }
+}
