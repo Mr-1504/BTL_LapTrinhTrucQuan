516a5ffe2270b6197f522feab74561c4fb1a8f6c
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-12 21:54:50 +0700

    Create PurchasedList.cs

---
+++ GUI/PurchasedIngredient/PurchasedList.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace GUI.PurchasedIngredient
+{
+    public partial class PurchasedList : Form
+    {
+        string _id;
+        BaseForm _baseForm;
+        public PurchasedList(string id, BaseForm @base)
+        {
+            InitializeComponent();
+            _id = id;
+            _baseForm = @base;
+        }
+
+        private void btnAdd_Click(object sender, System.EventArgs e)
+        {
+            DetailPurchaseedIngredient detail = new DetailPurchaseedIngredient(_id, _baseForm);
+            detail.Location = new System.Drawing.Point(0, 0);
+            detail.Show();
+            this.SendToBack();
+        }
+    }
+}
