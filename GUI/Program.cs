604d59f36e8d125093afbd008e184fee14b6a61a
Mr-1504 <truongvan.minh1504@gmail.com>
2024-10-13 21:53:37 +0700

    Update Program.cs

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -16,7 +16,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BaseForm("KH20240001"));
+            Application.Run(new LoadingForm());
         }
     }
 }
